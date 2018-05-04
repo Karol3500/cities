6deddd5e881d50fab687fef39ba60bf5350c3c5f
Karol Posila <karol.posila@nokia.com>
2018-05-04 22:11:55 +0200

    searching for countries done

--- src/main/java/org/posila/cities/cities/CountryController.java
+++ src/main/java/org/posila/cities/cities/CountryController.java
@@ -1,8 +1,10 @@
 package org.posila.cities.cities;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.posila.cities.cities.dao.ContinentDAO;
 import org.posila.cities.cities.dao.CountryDAO;
 import org.posila.cities.cities.entities.Continent;
+import org.posila.cities.cities.entities.ContinentsWrapper;
 import org.posila.cities.cities.entities.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -22,7 +26,7 @@ public class CountryController {
     @RequestMapping(value = "/country", method = RequestMethod.POST)
     public void addCountry(@RequestParam String continentName, @RequestParam String countryName) {
         Continent continent = continentDAO.getExistingOrNewContinent(continentName);
-        if(!continent.findCountry(countryName).isPresent()) {
+        if (!continent.findCountry(countryName).isPresent()) {
             continentDAO.save(continent.withCountry(new Country(countryName)));
         }
     }
@@ -30,10 +34,48 @@ public class CountryController {
     @RequestMapping(value = "/country", method = RequestMethod.DELETE)
     public void deleteCountry(@RequestParam String countryName) {
         Collection<Country> countries = countryDAO.findByName(countryName);
-        if(countries.isEmpty()){
+        if (countries.isEmpty()) {
             return;
         }
         countryDAO.cascadeDelete(countries);
+    }
+
+    @RequestMapping(value = "/country", method = RequestMethod.GET)
+    public String findCountry(@RequestParam(required = false) String continentName, @RequestParam String countryName) throws JsonProcessingException {
+        Collection<Continent> result = new ArrayList<>();
+        if (continentName == null) {
+            result.addAll(findCountryOnAllContinents(countryName));
+        } else {
+            result.addAll(findCountryOnContinent(continentName, countryName));
+        }
+        return new ContinentsWrapper(result).asJson();
+    }
+
+    private Collection<Continent> findCountryOnContinent(String continentName, String countryName) {
+        Collection<Continent> results = new ArrayList<>();
+        continentDAO.findByName(continentName)
+                .ifPresent(continent ->
+                        continent.findCountry(countryName).ifPresent(country -> results.add(
+                                prepareContinentWithtoutOtherCountries(country, continent))
+                        )
+                );
+        return results;
+    }
+
+    private Collection<Continent> findCountryOnAllContinents(String countryName) {
+        Collection<Country> countries = countryDAO.findByName(countryName);
+        Collection<Continent> results = new ArrayList<>();
+        for (Country country : countries) {
+            results.add(prepareContinentWithtoutOtherCountries(country,
+                    continentDAO.findByCountry(country)));
+        }
+        return results;
+    }
+
+    private Continent prepareContinentWithtoutOtherCountries(Country country, Continent parentContinent) {
+        Continent resultContinent = new Continent(parentContinent.getName());
+        resultContinent.getCountries().add(country);
+        return resultContinent;
     }
 
     @Autowired
--- src/test/java/org/posila/cities/cities/CountryControllerTest.java
+++ src/test/java/org/posila/cities/cities/CountryControllerTest.java
@@ -1,16 +1,22 @@
 package org.posila.cities.cities;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.posila.cities.cities.entities.City;
 import org.posila.cities.cities.entities.Continent;
+import org.posila.cities.cities.entities.ContinentsWrapper;
 import org.posila.cities.cities.entities.Country;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class CountryControllerTest extends ControllerTestSetup {
 
@@ -95,5 +101,42 @@ public class CountryControllerTest extends ControllerTestSetup {
         assertThat(cityDAO.findAll()).containsExactly(new City("New York"));
         assertThat(countryDAO.findAll()).containsExactlyInAnyOrder(
                 new Country("USA").withCity(new City("New York")), new Country("Finland"));
+    }
+
+    @Test
+    public void shouldFindCountriesByName() throws Exception {
+        continentDAO.save(new Continent("North America")
+                .withCountry(new Country("Canada").withCity(new City("Toronto")))
+                .withCountry(new Country("USA").withCity(new City("New York"))));
+        continentDAO.save(new Continent("Europe")
+                .withCountry(new Country("Finland"))
+                .withCountry(new Country("Canada").withCity(new City("Fake city"))));
+
+        MvcResult mvcResult = mockMvc.perform(get("/countries/country").contentType(MediaType.TEXT_PLAIN)
+                .param("countryName", "Canada")).andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo(
+                new ObjectMapper().writeValueAsString(new ContinentsWrapper(Arrays.asList(
+                        new Continent("North America").withCountry(new Country("Canada").withCity(new City("Toronto"))),
+                        new Continent("Europe").withCountry(new Country("Canada").withCity(new City("Fake city")))))));
+    }
+
+    @Test
+    public void shouldFindCountries_onGivenContinent() throws Exception {
+        continentDAO.save(new Continent("North America")
+                .withCountry(new Country("Canada").withCity(new City("Toronto")))
+                .withCountry(new Country("USA").withCity(new City("New York"))));
+        continentDAO.save(new Continent("Europe")
+                .withCountry(new Country("Finland"))
+                .withCountry(new Country("Canada").withCity(new City("Fake city"))));
+
+        MvcResult mvcResult = mockMvc.perform(get("/countries/country").contentType(MediaType.TEXT_PLAIN)
+                .param("countryName", "Canada")
+                .param("continentName", "North America")).andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo(
+                new ObjectMapper().writeValueAsString(new ContinentsWrapper(Collections.singletonList(
+                        new Continent("North America")
+                                .withCountry(new Country("Canada").withCity(new City("Toronto")))))));
     }
 }
